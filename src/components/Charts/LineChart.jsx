0477d344f29e1f84a620deb5e1bed7ef8ed128f8
Mr.Ben <42569624+1205BEN@users.noreply.github.com>
2022-05-21 22:21:32 +0700

    final complete

---
+++ src/components/Charts/LineChart.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, LineSeries, DateTime, Legend, Tooltip } from '@syncfusion/ej2-react-charts';
+
+import { lineCustomSeries, LinePrimaryXAxis, LinePrimaryYAxis } from '../../assets/dummy';
+import { useStateContext } from '../../contexts/ContextProvider';
+
+const LineChart = () => {
+
+  const {currentMode} = useStateContext();
+
+  return (
+    <ChartComponent
+      id="line-chart"
+      height="420px"
+      primaryXAxis={LinePrimaryXAxis}
+      primaryYAxis={LinePrimaryYAxis}
+      chartArea={{border:{width:0}}}
+      tooltip={{enable:true}}
+      background={currentMode === 'Dark' ? '#33337E' : '#fff'}
+    >
+      <Inject services={[LineSeries,DateTime,Legend,Tooltip]} />
+      <SeriesCollectionDirective>
+        {lineCustomSeries.map((item,index) => (
+          <SeriesDirective key={index} {...item} />
+        ))}
+      </SeriesCollectionDirective>
+    </ChartComponent>
+  )
+}
+
+export default LineChart
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
+import {ContextProvider} from './contexts/ContextProvider';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+ReactDOM.render(
+  <ContextProvider>
     <App />
-  </React.StrictMode>
-);
+  </ContextProvider>
+,document.getElementById('root'));
+
 
